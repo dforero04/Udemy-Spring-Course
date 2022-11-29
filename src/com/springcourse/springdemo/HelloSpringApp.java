1bfc283e8deda7269046532c71127a7a85de8ca3
dforero04 <dxforero@ualr.edu>
2022-11-29 15:35:17 -0600

    Injecting literal values

--- src/com/springcourse/springdemo/HelloSpringApp.java
+++ src/com/springcourse/springdemo/HelloSpringApp.java
@@ -10,7 +10,7 @@ public class HelloSpringApp {
 
         // retrieve bean from config file
         Coach theCoach = context.getBean("myCoach", Coach.class);
-        Coach swimCoach = context.getBean("mySwimmingCoach", SwimmingCoach.class);
+        SwimmingCoach swimCoach = context.getBean("mySwimmingCoach", SwimmingCoach.class);
 
         // Call methods on the bean
         System.out.println("Dependency Injection");
@@ -20,6 +20,8 @@ public class HelloSpringApp {
         System.out.println("Setter Injection");
         System.out.println(swimCoach.getDailyWorkout());
         System.out.println(swimCoach.getDailyFortune());
+        System.out.println(swimCoach.getEmailAddress());
+        System.out.println(swimCoach.getTeam());
 
         // Close the context
         context.close();
--- src/com/springcourse/springdemo/SwimmingCoach.java
+++ src/com/springcourse/springdemo/SwimmingCoach.java
@@ -2,6 +2,11 @@ package com.springcourse.springdemo;
 
 public class SwimmingCoach implements Coach {
 
+    // Class fields
+    private String emailAddress;
+    private String team;
+
+    // Class Injections
     private FortuneService fortuneService;
 
     // create a no-arg constructor
@@ -9,7 +14,26 @@ public class SwimmingCoach implements Coach {
         System.out.println("Swimming Coach: inside no-arg constructor");
     }
 
-    // Our Setter method (Setter Injection)
+    // Getters and Setters
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Swimming Coach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Swimming Coach: inside setter method - setTeam");
+        this.team = team;
+    }
+
+    // Our Setter methods (Setter Injection)
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("Swimming Coach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
